8b49ffff3b977aea95dcde9884b7aacf83d077a4
harshitabajpai1 <114719099+harshitabajpai1@users.noreply.github.com>
2024-11-12 21:18:13 +0530

    Cart.js

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function Cart() {
-  // Dummy cart data
+
   const cartItems = [
     { id: 1, name: 'Product 1', price: '$20', quantity: 2 },
     { id: 2, name: 'Product 2', price: '$30', quantity: 1 },
